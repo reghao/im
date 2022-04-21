4f0f2dec7111722ec077cd733fb9b08f0c4df440
reghao <reghaodev@gmail.com>
2022-04-21 09:16:46 +0800

    sql schema

---
+++ zzz/reghao_im_rdb.sql
@@ -0,0 +1,222 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost-mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50732
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : reghao_im_rdb
+
+ Target Server Type    : MySQL
+ Target Server Version : 50732
+ File Encoding         : 65001
+
+ Date: 21/04/2022 09:16:09
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for chat
+-- ----------------------------
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `chat_id` bigint(20) NOT NULL,
+  `chat_type` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for chat_dialog
+-- ----------------------------
+DROP TABLE IF EXISTS `chat_dialog`;
+CREATE TABLE `chat_dialog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `chat_id` bigint(20) NOT NULL,
+  `chat_type` int(10) NOT NULL,
+  `receiver_id` bigint(20) NOT NULL,
+  `is_disturb` bit(1) NOT NULL,
+  `is_online` bit(1) NOT NULL,
+  `is_robot` bit(1) NOT NULL,
+  `is_top` bit(1) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `unread_num` int(11) NOT NULL,
+  `last_send_user_id` bigint(20) NOT NULL,
+  `update_at` datetime NOT NULL,
+  `msg_text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for chat_record
+-- ----------------------------
+DROP TABLE IF EXISTS `chat_record`;
+CREATE TABLE `chat_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `record_id` bigint(20) DEFAULT NULL,
+  `chat_id` bigint(20) NOT NULL,
+  `sender_id` bigint(20) NOT NULL,
+  `msg_type` int(10) NOT NULL,
+  `is_mark` bit(1) NOT NULL,
+  `is_read` bit(1) NOT NULL,
+  `is_revoke` bit(1) NOT NULL,
+  `create_at` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sender_id` (`sender_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for emoticon
+-- ----------------------------
+DROP TABLE IF EXISTS `emoticon`;
+CREATE TABLE `emoticon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `media_id` int(11) NOT NULL,
+  `src` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_p0dbxwvogoamsrntdo066xlhj` (`media_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for file_message
+-- ----------------------------
+DROP TABLE IF EXISTS `file_message`;
+CREATE TABLE `file_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `record_id` bigint(20) NOT NULL,
+  `filename` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `suffix` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `size` bigint(20) NOT NULL,
+  `file_type` int(11) NOT NULL,
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `drive` int(11) NOT NULL,
+  `source` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sender_id` (`filename`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for setting
+-- ----------------------------
+DROP TABLE IF EXISTS `setting`;
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `keyboard_event_notify` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notify_cue_tone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `theme_bag_img` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `theme_color` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `theme_mode` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_p0dbxwvogoamsrntdo066xlhj` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for text_message
+-- ----------------------------
+DROP TABLE IF EXISTS `text_message`;
+CREATE TABLE `text_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `record_id` bigint(20) NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for user_account
+-- ----------------------------
+DROP TABLE IF EXISTS `user_account`;
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `mobile` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nickname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `platform` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_rfuvf3osnlaakifjdao0a8e2x` (`mobile`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for user_contact
+-- ----------------------------
+DROP TABLE IF EXISTS `user_contact`;
+CREATE TABLE `user_contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `friend_id` bigint(20) NOT NULL,
+  `nickname_remark` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `friend_status` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`) USING BTREE,
+  KEY `friend_id` (`friend_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for user_contact_record
+-- ----------------------------
+DROP TABLE IF EXISTS `user_contact_record`;
+CREATE TABLE `user_contact_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `request_user_id` bigint(20) NOT NULL,
+  `added_user_id` bigint(20) NOT NULL,
+  `friend_status` int(10) NOT NULL,
+  `friend_apply` int(10) NOT NULL,
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`request_user_id`) USING BTREE,
+  KEY `friend_id` (`added_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for user_profile
+-- ----------------------------
+DROP TABLE IF EXISTS `user_profile`;
+CREATE TABLE `user_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` bit(1) NOT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `update_time` datetime(6) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `nickname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` int(10) NOT NULL,
+  `motto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `online` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_rfuvf3osnlaakifjdao0a8e2x` (`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
